d8be041629f26854424ab190181de11d0cea849f
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-26 15:59:26 +0900

    ユーザー登録(ビュー、モデル)

---
+++ app/models/status.rb
@@ -0,0 +1,10 @@
+class Status < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'single' },
+    { id: 3, name: 'dating' },
+    { id: 4, name: 'merried' },
+  ]
+  include ActiveHash::Associations
+  has_many :users
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          validates :name, presence: true
          validates :comment, presence: true
-         validates :status_id, presence: true
+         validates :status_id
          validates :age, presence: true
-       
-          has_one_attached :image
+         has_one_attached :image
 end
