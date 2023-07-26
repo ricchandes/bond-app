eca88333ec38853b4afe081aec9beade378f7e2d
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-26 16:16:00 +0900

    新規ユーザー登録(ビュー、モデル、gem)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :name, presence: true
-         validates :comment, presence: true
-         validates :status_id
-         validates :age, presence: true
-         has_one_attached :image
+  validates :name, presence: true
+  validates :comment, presence: true
+  validates :age, presence: true
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
 end
