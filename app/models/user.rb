dd69a7e2dfb5adac842d98ce872b4b85edca06ba
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-27 13:38:09 +0900

    user一覧表示(ビュー)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :image
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
   validates :comment, presence: true
   validates :age, presence: true
-  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :status
