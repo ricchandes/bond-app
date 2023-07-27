496e3deae61dac25216c202f6d0aed565ffeb753
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-27 22:14:16 +0900

    like機能の実装(コントローラー)

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,13 @@
+class RelationshipsController < ApplicationController
+  def create
+    like = current_user.like_relationships.new(liked_id: params[:user_id])
+    like.save
+    redirect_to root_path
+  end
+
+  def destroy
+    like = current_user.like_relationships.find_by(liked_id: params[:user_id])
+    like.destroy
+    redirect_to root_path
+  end
+end
