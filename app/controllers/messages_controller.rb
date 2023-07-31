7f502e1f24efb594c98e78d5721bc1b634eb3c16
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-31 15:52:31 +0900

    メッセージの表示(コントローラー、ビュー)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,22 +1,21 @@
 class MessagesController < ApplicationController
   def new
     @message = Message.new
+    @room_user = RoomUser.find(params[:user_id])
+    @messages = @room_user.messages.includes(:user)
+    
   end
 
   def create
     @room_user = RoomUser.find(params[:user_id])
     @message = Message.create(message_params)
     if @message.save
-      redirect_to user_room_users_messages_path
+      redirect_to user_room_users_messages_path(@room_user)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def show
-    @messages = Message.includes(:user)
-    
-  end
 
   def message_params
     params.require(:message).permit(:text).merge(user_id: current_user.id, room_user_id: @room_user.id)
