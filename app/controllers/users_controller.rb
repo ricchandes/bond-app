5a606868119424dcf2bc4ac5c8863cfff74f0b99
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-08-03 15:21:28 +0900

    ログアウトエラー修正②

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def edit; end
 
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'users#index'
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy' 
+  end
   resources :users, only: %i[edit update show] do
     resource :relationships, only: %i[create destroy]
   end
